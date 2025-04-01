edaabb0f4517cfb6aaf5a7a17dba71b1c05a7ada
Saul Isaac Gonzalez <120429031+masterred8902@users.noreply.github.com>
2025-04-01 12:53:18 -0600

    versión 0.0

---
+++ login.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const ingresarBtn = document.querySelector(".Ingresar button");
+
+    ingresarBtn.addEventListener("click", () => {
+        window.location.href = "SGEI.html"; // Cambia esto por la URL de tu página
+    });
+});
